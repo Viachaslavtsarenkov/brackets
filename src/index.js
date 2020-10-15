8da09a515af94536e75ff1ddf019705c963122ac
Viacheslav Tsarenkov <tsarenkovslava@yandex.ru>
2020-10-15 18:48:41 +0300

    feat
    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = bracketsConfig.join("").replace(/,/g,"");
+  let index = 0;
+  let currentBrackets;
+  let n = str.length;
+  for(let i = 0; i < n; ++i){
+    if(index >= brackets.length-2)
+    index = 0;
+    while(str !== "" && index < brackets.length){
+    currentBrackets = brackets.substring(index ,index + 2);
+    console.log(currentBrackets);
+    if(str.indexOf(currentBrackets) >= 0){
+      str = str.replace(currentBrackets,'');
+      index += 2;
+      break;
+    } 
+    index += 2;
+  }
 }
+  if (str === "") return true;
+  return false;
+}
+   
